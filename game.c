27df1b61cfb2f752b65715d6fe46b4a44cf1be55
Emanuel <152207479+eeyieryi@users.noreply.github.com>
2025-03-19 22:59:01 +0400

    Block, blocks, rotation

--- game.c
+++ game.c
@@ -1,55 +1,212 @@
+#include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
 #include "raylib.h"
-
-#define NUM_BLOCKS 7
-#define NUM_TILES 8
-int blocks[NUM_BLOCKS][NUM_TILES] = {
-    {
-        1, 1, 1, 0,
-        0, 1, 0, 0,
-    },
-    {
-        0, 1, 1, 0,
-        1, 1, 0, 0,
-    },
-    {
-        1, 1, 0, 0,
-        0, 1, 1, 0,
-    },
-    {
-        1, 1, 1, 1,
-        0, 0, 0, 0,
-    },
-    {
-        1, 1, 1, 0,
-        1, 0, 0, 0,
-    },
-    {
-        1, 1, 1, 0,
-        0, 0, 1, 0,
-    },
-    {
-        1, 1, 0, 0,
-        1, 1, 0, 0,
-    },
-};
 
 #define NUM_LINES 20
 #define NUM_COLUMNS 10
 int board[NUM_LINES][NUM_COLUMNS];
-
 #define INITIALIZE_BOARD_WITH 0
 void ResetBoard() {
     for (int i = 0; i < NUM_LINES; i++)
         for (int j = 0; j < NUM_COLUMNS; j++)
             board[i][j] = INITIALIZE_BOARD_WITH;
 }
+
+#define MAX_ROTATIONS 4
+#define MAX_CELLS 16
+typedef struct Block {
+    int rowLength;
+    int colLength;
+    int cellLength; // row*col
+    int rotationLength;
+    int rotations[MAX_ROTATIONS][MAX_CELLS];
+} Block;
+
+const Block blockA = {
+    .rotationLength = 4,
+    .rowLength = 3,
+    .colLength = 3,
+    .cellLength = 9,
+    .rotations = {
+        {
+            1, 1, 1,
+            0, 1, 0,
+            0, 0, 0,
+        },
+        {
+            1, 0, 0,
+            1, 1, 0,
+            1, 0, 0,
+        },
+        {
+            0, 0, 0,
+            0, 1, 0,
+            1, 1, 1,
+        },
+        {
+            0, 1, 0,
+            1, 1, 0,
+            0, 1, 0,
+        },
+    },
+};
+
+const Block blockB = {
+    .rowLength = 3,
+    .colLength = 3,
+    .cellLength = 9,
+    .rotationLength = 4,
+    .rotations = {
+        {
+            0, 0, 0,
+            1, 1, 1,
+            0, 0, 1,
+        },
+        {
+            0, 1, 1,
+            0, 1, 0,
+            0, 1, 0,
+        },
+        {
+            1, 0, 0,
+            1, 1, 1,
+            0, 0, 0,
+        },
+        {
+            0, 1, 0,
+            0, 1, 0,
+            1, 1, 0,
+        },
+    },
+};
+
+const Block blockC = {
+    .rowLength = 3,
+    .colLength = 3,
+    .cellLength = 9,
+    .rotationLength = 4,
+    .rotations = {
+        {
+            0, 0, 0,
+            1, 1, 0,
+            0, 1, 1,
+        },
+        {
+            0, 0, 1,
+            0, 1, 1,
+            0, 1, 0,
+        },
+        {
+            0, 0, 0,
+            1, 1, 0,
+            0, 1, 1,
+        },
+        {
+            0, 0, 1,
+            0, 1, 1,
+            0, 1, 0,
+        },
+    },
+};
+
+const Block blockD = {
+    .rotationLength = 1,
+    .rowLength = 2,
+    .colLength = 2,
+    .cellLength = 4,
+    .rotations = {
+        {
+            1, 1,
+            1, 1,
+        },
+    },
+};
+
+const Block blockE = {
+    .rotationLength = 4,
+    .rowLength = 3,
+    .colLength = 3,
+    .cellLength = 9,
+    .rotations = {
+        {
+            0, 0, 0,
+            0, 1, 1,
+            1, 1, 0,
+        },
+        {
+            0, 1, 0,
+            0, 1, 1,
+            0, 0, 1,
+        },
+        {
+            0, 0, 0,
+            0, 1, 1,
+            1, 1, 0,
+        },
+        {
+            0, 1, 0,
+            0, 1, 1,
+            0, 0, 1,
+        },
+    },
+};
+
+const Block blockF = {
+    .rowLength = 3,
+    .colLength = 3,
+    .cellLength = 9,
+    .rotationLength = 4,
+    .rotations = {
+        {
+            0, 0, 0,
+            1, 1, 1,
+            1, 0, 0,
+        },
+        {
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 1,
+        },
+        {
+            0, 0, 1,
+            1, 1, 1,
+            0, 0, 0,
+        },
+        {
+            1, 1, 0,
+            0, 1, 0,
+            0, 1, 0,
+        },
+    },
+};
+
+const Block blockG = {
+    .rotationLength = 2,
+    .rowLength = 4,
+    .colLength = 4,
+    .cellLength = 16,
+    .rotations = {
+        {
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+            0, 0, 0, 0,
+        },
+        {
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+        },
+     },
+};
+
+#define NUM_BLOCKS 7
+Block blocks[NUM_BLOCKS] = { blockA, blockB, blockC, blockD, blockE, blockF, blockG };
 
 int main(void) {
     int windowWidth = 640;
@@ -61,11 +218,11 @@ int main(void) {
     InitWindow(windowWidth, windowHeight, windowTitle);
     SetTargetFPS(120);
 
-
-    int *currentBlock = blocks[0];
+    int currentBlockNumber = 0;
+    Block *currentBlock = &blocks[currentBlockNumber];
+    int currentRotation = 0;
     int currentX = 1;
     int currentY = 1;
-
 
     double seconds = 0;
 
@@ -102,31 +259,52 @@ int main(void) {
             currentX += 1;
         }
 
+        if (IsKeyPressed(KEY_UP)) {
+            currentRotation = (currentRotation + 1) % (currentBlock)->rotationLength;
+        }
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            currentBlockNumber = (currentBlockNumber + 1) % NUM_BLOCKS;
+            currentBlock = &blocks[currentBlockNumber];
+        }
+
         BeginDrawing();
         {
             ClearBackground(GetColor(0x000000FF));
 
             {
                 int tileWidth = 20;
-                int startOffsetX = windowWidth - tileWidth * 5;
-                int startOffsetY = 20;
+                int originalOffsetX = windowWidth - tileWidth * 10;
+                int originalOffsetY = 20;
+                int startOffsetX = originalOffsetX;
+                int startOffsetY = originalOffsetY;
                 int offsetX = startOffsetX;
                 int offsetY = startOffsetY;
+                Block *block;
                 for (int i = 0; i < NUM_BLOCKS; i++) {
-                    for (int j = 0; j < NUM_TILES; j++) {
-                        if (blocks[i][j] == 1) {
+                    block = &blocks[i];
+                    offsetX = startOffsetX;
+                    offsetY = startOffsetY;
+                    for (int j = 0; j < (block)->cellLength; j++) {
+                        if ((block)->rotations[0][j] == 1) {
                             DrawRectangle(offsetX, offsetY, tileWidth, tileWidth, RED);
                         } else {
                             DrawRectangle(offsetX, offsetY, tileWidth, tileWidth, BLUE);
                         }
                         offsetX += tileWidth+1;
-                        if (j == 3) {
+                        if (j+1 == (block)->cellLength) break;
+                        if ((j+1) % (block)->colLength == 0) {
                             offsetX = startOffsetX;
                             offsetY += tileWidth+1;
                         }
                     }
-                    offsetX = startOffsetX;
-                    offsetY += tileWidth*2+1;
+                    if ((i+1) % 2 == 0) {
+                        startOffsetX = originalOffsetX;
+                        originalOffsetY = offsetY + (tileWidth)*3;
+                    } else {
+                        startOffsetX += (offsetX-originalOffsetX)+tileWidth;
+                    }
+                    startOffsetY = originalOffsetY;
                 }
 
                 startOffsetX = tileWidth * 9;
@@ -159,12 +337,12 @@ int main(void) {
                 startOffsetY = (tileWidth*1.5+1)+(currentY*(tileWidth+1));
                 offsetX = startOffsetX;
                 offsetY = startOffsetY;
-                for (int i = 0; i < NUM_TILES; i++) {
-                    if (currentBlock[i] == 1) {
+                for (int i = 0; i < (currentBlock)->cellLength; i++) {
+                    if ((currentBlock)->rotations[currentRotation%(currentBlock)->rotationLength][i] == 1) {
                         DrawRectangle(offsetX, offsetY, tileWidth, tileWidth, RED);
                     }
                     offsetX += tileWidth+1;
-                    if (i == 3) {
+                    if ((i+1) % (currentBlock)->colLength == 0) {
                         offsetY += tileWidth+1;
                         offsetX = startOffsetX;
                     }
